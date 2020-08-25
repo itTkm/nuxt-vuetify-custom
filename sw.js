eaf9b32823c7c080eee88c4fecaa5a8cc76ad3f6
itTkm <itTkm@users.noreply.github.com>
2020-08-25 23:46:11 +0000

    deploy: 01e8d501e10eabe4cccdc5b682cb38b0349b8d2e

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/itTkm/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/itTkm/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt-vuetify-custom/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt-vuetify-custom/'), new workbox.strategies.NetworkFirst ({}), 'GET')
